7691c4738d9bd73d9ba6af597541cbb0611f8c98
Ethan Watson <ethanwatson@Ethans-MacBook-Pro.local>
2020-07-09 18:15:17 -0500

    attempt to deploy express as react

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Posts from './components/Posts';
 
 class App extends Component {
   state = {users: []}
 
   componentDidMount() {
-    fetch('/posts')
+    fetch('/')
       .then(res => res.json())
       .then(users => this.setState({ users }));
   }
