85c9e76ac16776c7fdb14b4d3e49ed0b48421376
Ethan Watson <ethanwatson@Ethans-MacBook-Pro.local>
2020-07-12 16:41:31 -0500

    broke connection to db into its own file in model

--- routes/posts.js
+++ routes/posts.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var pgp = require('pg-promise')(/* options */)
-const cn = {
-    host: 'ec2-34-225-162-157.compute-1.amazonaws.com',
-    port: 5432,
-    database: 'd9as4j31v8lhig',
-    user: 'djodxvfvrmjrsi',
-    password: '5e1822c359c9321894579370db30f1a62189a02ef7f1a392d5c8ffbd0250c8a2',
-    ssl: { rejectUnauthorized: false },
-    max: 30 // use up to 30 connections
-};
-const db = pgp(cn);
-
+var db = require('../models/connection')
 
 
 /* GET home page. */
